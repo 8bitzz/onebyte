e5c826b14c7a198d4ba476ab60797c53a044bb18
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-10-02 15:16:17 +0700

    Change logo for website

--- frontend/components/navbar/NavBar.jsx
+++ frontend/components/navbar/NavBar.jsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="fixed w-full z-40 top-0 bg-white px-20 py-8 font-extralight tracking-widest uppercase">
+    <div className="fixed w-full z-40 top-0 bg-white px-20 py-8 font-extralight tracking-widest">
       <div className="h-full flex justify-between items-end py-3">
         <Link href="/">
-          <div className="flex h-full justify-center items-center cursor-pointer">
-            <span className="text-2xl cursor-pointer font-bold">Onebyte</span>
+          <div className="flex flex-row h-full justify-center items-end cursor-pointer">
+            <img src="/logo.png" className="h-10 mr-1"></img>
+            <span className="text-2xl cursor-pointer font-semibold lowercase">Onebyte</span>
           </div>
         </Link>
-        <div className="flex h-full justify-end items-center cursor-pointer">
+        <div className="flex h-full justify-end items-center cursor-pointer uppercase">
           <Link href="/blogs">
             <div className="ml-10">blogs</div>
           </Link>
